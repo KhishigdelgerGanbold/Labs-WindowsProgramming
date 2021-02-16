4f24844c27ad6576625756a8851b74e5fe29bb39
16B1SAS3244 Khishigdelger <16B1SAS3244@stud.num.edu.mn>
2021-02-16 19:06:40 +0800

    Project Files for C# Console App

---
+++ CSharpConsoleApp - Windows Programming/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khishigdelger
+{
+    class Task1
+    {
+        private string name; // Private field
+        public string NameValue // NameValue - g ashiglan private field - iin utgiig oorchlono.
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+}
---
+++ CSharpConsoleApp - Windows Programming/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsoleApp___Windows_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Khishigdelger.Task1 myObj = new Khishigdelger.Task1();
+            Console.WriteLine("Enter your username: ");
+            myObj.NameValue = Console.ReadLine();
+            Console.WriteLine(myObj.NameValue);
+            Console.WriteLine("Press enter to exit. . .");
+            Console.ReadLine();
+
+        }
+    }
+}
